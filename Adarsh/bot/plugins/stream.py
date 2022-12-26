abb4db8e17ae6deca296e9cf27f18a647300ee74
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-12-26 13:26:37 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -24,7 +24,7 @@ pass_db = Database(Var.DATABASE_URL, "ag_passwords")
 async def login_handler(c: Client, m: Message):
     try:
         try:
-            ag = await m.reply_text("Now send me password.\n\n for 𝙿𝙰𝚂𝚂𝚆𝙾𝚁𝙳 send  𝙵𝚁𝙴𝙴 𝙳𝙼   \n\n (for running bot need contributions..unwanted message = ban + report)")
+            ag = await m.reply_text("Now send me password.\n\n for 𝙿𝙰𝚂𝚂𝚆𝙾𝚁𝙳 send  𝙵𝚁𝙴𝙴 𝙳𝙼 @R_KOHLI  \n\n (for running bot need contributions..unwanted message = ban + report)")
             _text = await c.listen(m.chat.id, filters=filters.text, timeout=90)
             if _text.text:
                 textp = _text.text
@@ -50,7 +50,7 @@ async def private_receive_handler(c: Client, m: Message):
     if MY_PASS:
         check_pass = await pass_db.get_user_pass(m.chat.id)
         if check_pass== None:
-            await m.reply_text("ʟᴏɢɪɴ ғɪʀsᴛ ᴜsɪɴɢ /login ᴄᴍᴅ(ᴛʏᴘᴇ /login)  \n\n for password free DM @R_KOHLI \n\n (for running bot need contributions..unwanted message = ban + report)")
+            await m.reply_text("ʟᴏɢɪɴ ғɪʀsᴛ ᴜsɪɴɢ /login ᴄᴍᴅ(ᴛʏᴘᴇ /login)  \n\n for 𝙿𝙰𝚂𝚂𝚆𝙾𝚁𝙳 send  𝙵𝚁𝙴𝙴 𝙳𝙼 @R_KOHLI \n\n (for running bot need contributions..unwanted message = ban + report)")
             return
         if check_pass != MY_PASS:
             await pass_db.delete_user(m.chat.id)
